7a2e156f1b246ca3a904cf32ec31c51cc1c198e0
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-11-18 09:32:33 -0700

    Made Project Base

---
+++ app/src/main/java/inventory/controller/Controller.java
@@ -0,0 +1,11 @@
+package inventory.controller;
+
+public class Controller {
+   public Controller(){
+
+   }
+
+   public void start(){
+      System.out.println("Hello World!");
+   }
+}
---
+++ app/src/main/java/inventory/controller/Runner.java
@@ -0,0 +1,10 @@
+package inventory.controller;
+
+import inventory.controller.Controller;
+
+public class Runner {
+   public static void main(String[] args){
+      Controller controller = new Controller();
+      controller.start();
+   }
+}
