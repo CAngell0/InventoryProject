9d288ffc0abe8b027ff504106653cec25b582a2c
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-12-05 09:25:37 -0700

    Added file reading methods for other items in model

--- app/src/main/java/inventory/controller/IOController.java
+++ app/src/main/java/inventory/controller/IOController.java
@@ -6,9 +6,14 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-import inventory.model.ClothingItem;
+import inventory.model.*;
 
 public class IOController {
+   /**
+    * Read all the ClothingItems from a given CSV dataset
+    * @param path File path to CSV dataset
+    * @return A list of ClothingItem
+    */
    public static ArrayList<ClothingItem> readClothingItemsFromFile(String path){
       ArrayList<ClothingItem> clothes = new ArrayList<ClothingItem>();
 
@@ -34,5 +39,68 @@ public class IOController {
       }
 
       return clothes;
+   }
+
+   /**
+    * Read all the HosptialFood items from a given CSV dataset
+    * @param path File path to CSV dataset
+    * @return A list of HospitalFood
+    */
+    public static ArrayList<HospitalFood> readHospitalFoodFromFile(String path){
+      ArrayList<HospitalFood> hospitalFood = new ArrayList<HospitalFood>();
+
+      try (Scanner fileScanner = new Scanner(new File(path))){
+         String foodLine = fileScanner.nextLine();
+
+         while (fileScanner.hasNextLine()){
+            foodLine = fileScanner.nextLine();
+            String[] lineContents = foodLine.split(",");
+
+            hospitalFood.add(new HospitalFood(
+               lineContents[0],
+               lineContents[1],
+               Integer.parseInt(lineContents[2]),
+               Boolean.parseBoolean(lineContents[3]),
+               Double.parseDouble(lineContents[4])
+            ));
+         }
+      } catch (FileNotFoundException error){
+         System.out.println("Error reading hospital food items");
+         System.err.println(error.getMessage());
+      }
+
+      return hospitalFood;
+   }
+
+   /**
+    * Read all the HospitalPatients from a given CSV dataset
+    * @param path File path to CSV dataset
+    * @return A list of ClothingItem
+    */
+    public static ArrayList<HospitalPatient> readHospitalPatientsFromFile(String path){
+      ArrayList<HospitalPatient> patients = new ArrayList<HospitalPatient>();
+
+      try (Scanner fileScanner = new Scanner(new File(path))){
+         String patientLine = fileScanner.nextLine();
+
+         while (fileScanner.hasNextLine()){
+            patientLine = fileScanner.nextLine();
+            String[] lineContents = patientLine.split(",");
+
+            patients.add(new HospitalPatient(
+               lineContents[0],
+               lineContents[1],
+               Boolean.parseBoolean(lineContents[2]),
+               Integer.parseInt(lineContents[3]),
+               Integer.parseInt(lineContents[4]),
+               Double.parseDouble(lineContents[5])
+            ));
+         }
+      } catch (FileNotFoundException error){
+         System.out.println("Error reading hospital patients");
+         System.err.println(error.getMessage());
+      }
+
+      return patients;
    }
 }
