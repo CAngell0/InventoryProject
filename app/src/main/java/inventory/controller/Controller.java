ecb87ee9c78b6ed827994f73ea86b7a8fc63d886
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-12-05 08:47:30 -0700

    Added some accumulator methods for the types and reworked the discount method

--- app/src/main/java/inventory/controller/Controller.java
+++ app/src/main/java/inventory/controller/Controller.java
@@ -3,6 +3,9 @@ package inventory.controller;
 import inventory.view.Popup;
 
 import inventory.model.ClothingItem;
+import inventory.model.HospitalFood;
+
+import java.util.ArrayList;
 
 public class Controller {
    private Popup view;
@@ -23,12 +26,47 @@ public class Controller {
     * @return The newly discounted array of ClothesItem
     */
    private ClothingItem[] applyDiscountTo(String clothingType, double discount, ClothingItem[] clothes){
-      for (ClothingItem item : clothes){
-         if (item.getClothingType().equals(clothingType)){
-            double currentPrice = item.getPrice();
-            item.setPrice(currentPrice * discount);
+      ArrayList<ClothingItem> discountedClothes = new ArrayList<ClothingItem>();
+      for (int index = 0; index < clothes.length; index++){
+         if (clothes[index].getClothingType().equalsIgnoreCase(clothingType)){
+            discountedClothes.add(clothes[index]);
+            discountedClothes.get(discountedClothes.size() - 1).setPrice(clothes[index].getPrice() * discount);
          }
       }
-      return clothes;
+
+      ClothingItem[] returnedList = new ClothingItem[discountedClothes.size()];
+      for (int index = 0; index < returnedList.length; index++){
+         returnedList[index] = discountedClothes.get(index);
+      }
+      return returnedList;
+   }
+
+   /**
+    * Counts the amount of clothe items that are of a specific type
+    * @param type The type to search by
+    * @param clothes Array of clothes
+    * @return An integer count of the clothing type
+    */
+   private int countClothingType(String type, ClothingItem[] clothes){
+      int count = 0;
+      for (ClothingItem item: clothes){
+         if (item.getClothingType().equalsIgnoreCase(type)) count++;
+      }
+      return count;
+   }
+
+   /**
+    * Counts the amount of vegan foods in a given list of HosptialFood items
+    * @param food Array of HostpitalFood
+    * @return An integer count of hospital food that is vegan
+    */
+   private int countVeganFood(HospitalFood[] food){
+      int count = 0;
+
+      for (HospitalFood foodItem : food){
+         if (foodItem.getIsVegan()) count++;
+      }
+
+      return count;
    }
 }
