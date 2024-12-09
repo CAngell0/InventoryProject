235dccab56382b18f4d12ff8b04932a4a1cc1cc7
CAngell0 <carsonjesseangell.learn@gmail.com>
2024-12-09 08:41:00 -0700

    Added modifier and getters methods for the data set arrays

--- app/src/main/java/inventory/controller/Controller.java
+++ app/src/main/java/inventory/controller/Controller.java
@@ -8,6 +8,8 @@ import inventory.model.HospitalPatient;
 
 import java.util.ArrayList;
 
+import com.google.common.net.HostAndPort;
+
 public class Controller {
    private Popup view;
 
@@ -24,22 +26,48 @@ public class Controller {
     * @param clothingType The type of clothing
     * @param discount The discount to use
     * @param clothes The array of clothing to apply discount to
-    * @return The newly discounted array of ClothesItem
     */
-   private ClothingItem[] applyDiscountTo(String clothingType, double discount, ClothingItem[] clothes){
-      ArrayList<ClothingItem> discountedClothes = new ArrayList<ClothingItem>();
-      for (int index = 0; index < clothes.length; index++){
-         if (clothes[index].getClothingType().equalsIgnoreCase(clothingType)){
-            discountedClothes.add(clothes[index]);
-            discountedClothes.get(discountedClothes.size() - 1).setPrice(clothes[index].getPrice() * discount);
+   private void applyDiscountTo(String clothingType, double discount, ClothingItem[] clothes){
+      for (ClothingItem currentClothingItem : clothes){
+         if (currentClothingItem.getClothingType().equalsIgnoreCase(clothingType)){
+            double currentPrice = currentClothingItem.getPrice();
+            currentPrice = currentPrice * discount;
+            currentClothingItem.setPrice(currentPrice);
          }
       }
+   }
 
-      ClothingItem[] returnedList = new ClothingItem[discountedClothes.size()];
-      for (int index = 0; index < returnedList.length; index++){
-         returnedList[index] = discountedClothes.get(index);
+   /**
+    * Applies a discount to a given hospital food type
+    * @param foodName Name of the food to search by
+    * @param discount Discount to apply
+    * @param hospitalFood The array of hospital food to apply discount to
+    */
+   private void applyDiscountTo(String foodName, double discount, HospitalFood[] hospitalFood){
+      for (HospitalFood currentFood : hospitalFood){
+         if (currentFood.getFoodName().equalsIgnoreCase(foodName)){
+            double currentPrice = currentFood.getPrice();
+            currentPrice = currentPrice * discount;
+            currentFood.setPrice(currentPrice);
+         }
       }
-      return returnedList;
+   }
+
+   /**
+    * Applies a discount to the price of stay for patients in a certain age range.
+    * @param minAge Minimum age of patients
+    * @param maxAge Maximum age of patients
+    * @param discount Discount to apply
+    * @param hospitalPatients The array of hospital patients to apply discount to
+    */
+   private void applyDiscountTo(int minAge, int maxAge, double discount, HospitalPatient[] hospitalPatients){
+      for (HospitalPatient currentPatient : hospitalPatients){
+         if (currentPatient.getAge() >= minAge && currentPatient.getAge() <= maxAge){
+            double currentPrice = currentPatient.getPriceOfStay();
+            currentPrice = currentPrice * discount;
+            currentPatient.setPriceOfStay(currentPrice);
+         }
+      }
    }
 
    /**
@@ -57,20 +85,28 @@ public class Controller {
    }
 
    /**
-    * Counts the amount of vegan foods in a given list of HosptialFood items
-    * @param food Array of HostpitalFood
-    * @return An integer count of hospital food that is vegan
+    * Count the amount of food that matches a specific food type
+    * @param foodName The name of the food to query by
+    * @param food The array of hospital food items to search in
+    * @return A count of how many food items of that type are in the array
     */
-   private int countVeganFood(HospitalFood[] food){
+   private int countFoodType(String foodName, HospitalFood[] food){
       int count = 0;
 
       for (HospitalFood foodItem : food){
-         if (foodItem.getIsVegan()) count++;
+         if (foodItem.getFoodName().equalsIgnoreCase(foodName)) count++;
       }
 
       return count;
    }
 
+   /**
+    * Count the amount of patients in a certain age range
+    * @param minAge The minimum age to search by
+    * @param maxAge The maximum age to search by
+    * @param patients The array of patients to search in
+    * @return A count of patients that are within the designated age range
+    */
    private int countPatientsInAgeRange(int minAge, int maxAge, HospitalPatient[] patients){
       int count = 0;
       
